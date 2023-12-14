1904a8c9143c235dc50ad1ac8157dcb1e0ce9d5f
Ruslan Senkin <rsenkin24@gmail.com>
2023-12-14 10:15:17 +0200

    override toString in Car class

--- src/Car.java
+++ src/Car.java
@@ -5,4 +5,14 @@ public class Car extends Vehicle {
         super(brand, year);
         this.model = model;
     }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Brand : %s Model : %s Year : %d%n",
+        this.getBrand(),this.getModel(), this.getYear());
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,8 +2,18 @@ public class Vehicle {
     private String brand;
     private int year;
 
+
+
     public Vehicle(String brand, int year) {
         this.brand = brand;
         this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
