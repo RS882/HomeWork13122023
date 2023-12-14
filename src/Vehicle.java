13f9ef337073eb2649cc1b8b8085d755599bea1e
Ruslan Senkin <rsenkin24@gmail.com>
2023-12-14 10:06:09 +0200

    add Vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+    private String brand;
+    private int year;
+
+    public Vehicle(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+}
